b08c0a021028801576c4965bb81b575ea4eac815
Kelebra <ricardokelebra@gmail.com>
2025-05-27 15:21:01 -0600

    xd

--- api/db.js
+++ api/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const db = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: '123456',
+  password: 'Ricardomma28',
   database: 'encuestas_app',
   port: 3306
 });
